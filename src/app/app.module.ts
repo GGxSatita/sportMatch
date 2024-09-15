bc2bc84f42d7ec48905749510489873638d789fe
Iván Carreño <105950912+GGxSatita@users.noreply.github.com>
2024-09-15 13:45:39 -0300

    "conexion a firebase"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,22 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+//imports necesarios para poder conectar firebase
+import { environment } from 'src/environments/environment';
+import {AngularFireAuthModule} from '@angular/fire/compat/auth'
+import { AngularFireModule } from '@angular/fire/compat';
+
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    AngularFireAuthModule, //AuthModule
+    AngularFireModule.initializeApp(environment.firebaseConfig)
+
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
